07dafbde1426822aacb2a6b21d61c83c41bdcebf
R3vii <revi.pingwin@gmail.com>
2025-03-04 09:34:53 +0100

    Update main-page-script.js

--- main-page-script.js
+++ main-page-script.js
@@ -1,4 +1,3 @@
-// Funkcja do przełączania widoczności listy
 function toggleList() {
     const list = document.getElementById('hidden-list');
     list.style.display = list.style.display === 'none' || list.style.display === '' ? 'block' : 'none';
