f4c6ac4b4307c6c6e4336f32069adcf55597ba1d
R3vii <revi.pingwin@gmail.com>
2025-03-05 04:00:16 +0100

    Add files via upload

--- main-page-script.js
+++ main-page-script.js
@@ -1,3 +1,4 @@
+// Funkcja do przełączania widoczności listy
 function toggleList() {
     const list = document.getElementById('hidden-list');
     list.style.display = list.style.display === 'none' || list.style.display === '' ? 'block' : 'none';
